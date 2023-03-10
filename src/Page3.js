48840c00ee898e25413e6f0faaf04c75d459f929
amrit2611 <dhandhariaamrit@gmail.com>
2023-03-10 16:28:22 +0530

    Created basic ui for Page3

--- src/Page3.js
+++ src/Page3.js
@@ -1,56 +1,85 @@
 import React from 'react'
 
+import shape1 from "./images/shape1.png"
+import shape2 from "./images/shape2.png"
+import shape3 from "./images/shape3.png"
+import skill1 from "./images/skill1.png"
+import skill2 from "./images/skill2.png"
+import line from "./images/line1.png"
+
+const sampletext = "Lorem mpsum dolor smt amet, consectetur admpmscmng elmt, sed do emusmod tempor mncmdmdunt ut labore et dolore magna almqua. Ut enmm ad mmnmm venmam, qums nostrud exercmtatmon ullamco laborms nmsm ut almqump ex ea commodo.Lorem mpsudolor smt amet, consectetur admpmscmng elmt, sed do emusmod tempor mncmdmdunt ut labore et dolore magna almqua. Ut enmm ad mmnmm venmam, qums nostrud exercmtatmon ullamco laborms nmsm ut almqump ex ea commodo venmam, qums nostrud exercmtatmon."
+
 function Page3() {
   return (
     <>
-      {/* <div className=" absolute parent-grid grid grid-cols-2 gap-10 border-solid border-black h-auto border-4 p-24 mx-48 font-sans">
-        <img src={shape1} className="absolute top-0 left-6 z-0 w-96" />
+      <div className=" absolute parent-grid grid grid-cols-2 gap-6 border-solid border-black h-auto border-4 pr-20 pl-16 pt-28 mx-48 font-sans">
+        <img src={shape1} className="absolute top-0 left-0 z-0 w-96" />
         <img src={shape2} className="absolute bottom-0 z-0" />
         <img src={shape3} className="absolute bottom-0 right-0 z-0" />
-        <img className="profile-image row-span-1 w-60 align-middle mr-auto mb-8 z-10" src={profileImages} alt='Profile Image' />
-        <div className="intro row-span-2 mt-28 mb-4">
-          <h1 className="font-bold text-6xl my-2">Devon Lane</h1>
-          <h3 className="font-bold text-3xl text-gray-600 my-5">Graphic Designer</h3>
-          <p className=" mt-2 font-medium">{profileDetails}</p>
+
+
+
+        <div className="language row-span-1 h-min font-medium my-10">
+          <h2 className="font-bold text-4xl mb-6">Language</h2>
+          <h2 className="font-bold text-xl my-3">English</h2>
+          <img src={skill1} />
+          <h2 className="font-bold text-xl my-3 ">Hindi</h2>
+          <img src={skill2} />
+          <h2 className="font-bold text-xl my-3 ">French</h2>
+          <img src={skill1} />
         </div>
-        <div className="contact row-span-2 font-medium mt-2">
-          <h2 className="font-semibold text-4xl mb-8">Contact</h2>
-          <div className="my-6"><img src={mobileLogo} alt="mobile" className='inline w-3 mr-5' /><span>+91-855-570-4521</span></div>
-          <div className="my-6"><img src={mailLogo} alt="mail" className='inline w-4 mr-5' /><span>devonlane@gmail.com</span></div>
-          <div className="my-6"><img src={websiteLogo} alt="website" className='inline w-4 mr-5' /><span>www.devonlane.com</span></div>
-          <div className="my-6"><img src={locationLogo} alt="location" className='inline w-4 mr-5' /><span>52 Reservoir Singapore</span></div>
-          <div className="my-6"><img src={linkedinLogo} alt="linkedin" className='inline w-4 mr-5' /><span>www.example1.com</span></div>
-          <div className="my-6"><img src={nationalityLogo} alt="nationality" className='inline w-4 mr-5' /><span> Indian</span></div>
-          <div className="my-6"><img src={birthdateLogo} alt="birthdate" className='inline w-4 mr-5' /><span>Jan 15, 1996</span></div>
+
+
+        <div className="projects row-span-4 mt-5">
+          <h1 className="font-bold text-4xl mb-4">Projects</h1>
+          <h3 className="font-semibold text-3xl my-2">Project Title</h3>
+          <p className="my-4 font-normal">Jan 2022 - Ongoing</p>
+          <h3 className="font-bold text-2xl my-2">Client name</h3>
+          <p className=" mt-2 font-medium">{sampletext}</p>
+          <img src={line} className="my-8 h-1"></img>
+          <h3 className="font-semibold text-3xl my-2">Project Title</h3>
+          <p className="my-4 font-normal">Jan 2020 - Jan 2021</p>
+          <h3 className="font-bold text-2xl my-2">Client name</h3>
+          <p className=" mt-2 font-medium">{sampletext}</p>
         </div>
-        <div className="education row-span-2 my-2 font-medium">
-          <h2 className="font-semibold text-4xl mb-6">Education</h2>
-          <p className="my-4 font-normal">Jun 2019 - Jun 2020</p>
-          <h2 className="font-medium text-2xl mb-4">Masters in Graphic Design</h2>
-          <p className="font-normal">University of Chicago | City | Country</p>
-          <ul className="list-disc ml-8  my-2"><li>It's very important for the patient to be patient, and the patient will be followed.</li></ul>
-          <p className="font-normal">June 2018 - June 2019</p>
-          <h2 className="font-medium text-2xl my-4">BA in Graphic Design</h2>
-          <p className="font-normal">University of Chicago | City | Country</p>
-          <ul className="list-disc ml-8 my-2"><li>It's very important for the patient to be patient, and the patient will be followed.</li></ul>
-        </div>
-        <div className="skill row-span-2 my-4 z-10">
-          <h2 className="font-semibold text-4xl my-8">Skills</h2>
-          <h3 className="text-2xl font-semibold mb-4">Skill 1</h3>
-          <h3 className="text-2xl font-semibold my-4">Skill 2</h3>
-          <h3 className="text-2xl font-semibold my-4">Skill 3</h3>
-          <h3 className="text-2xl font-semibold my-4">Skill 4</h3>
-          <h3 className="text-2xl font-semibold my-4">Skill 5</h3>
-        </div>
-        <div className="achievements row-span-1 font-medium">
-          <h2 className="font-semibold text-4xl mb-10">Achievements</h2>
-          <ul className="list-disc ml-8 my-4">
-            <li className="my-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>
-            <li className="my-3">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquea commodo consequat veniam, quis</li>
-            <li className="my-3">Ut enim ad minim veniam, quis nostrud.</li>
+
+
+
+
+        <div className="internships row-span-2 my-2 mr-6 h-min font-medium">
+          <h2 className="font-bold text-4xl mb-4">Internships</h2>
+          <h2 className="font-semibold text-2xl mb-2">Internship 1</h2>
+          <p className="my-2 font-normal">Jan 2019 - Jun 2019</p>
+          <p className="font-semibold">Company 1 | City | Country</p>
+          <p className="my-2 font-normal">URL : www.example1.com</p>
+          <ul className="list-disc ml-8 my-2">
+            <li>It's very important for the patient to be patient, and the patient will be followed.</li><li>It's very important for the patient to be patient, and the patient will be followed.</li>
           </ul>
         </div>
-      </div> */}
+
+
+        <div className="interests row-span-1 h-min my-4 mb-10 z-10">
+          <h2 className="font-bold text-4xl mt-6 mb-6">Personal Interests</h2>
+          <ul className="list-disc ml-8  my-2">
+            <li className="my-1">It's very important.</li>
+            <li className="my-1">It's very important.</li>
+            <li className="my-1">It's very important.</li>
+            <li className="my-1">It's very important.</li>
+            <li className="my-1">It's very important.</li>
+            <li className="my-1">It's very important.</li>
+          </ul>
+        </div>
+
+        <div className="row-span-1"></div>
+
+        <div className="training row-span-1 mb-20 mr-6 font-medium">
+          <h2 className="font-bold text-4xl mb-4">Training</h2>
+          <h2 className="font-semibold text-2xl mb-2">Training Title</h2>
+          <p className="my-2 font-normal">Jan 2018 - Jun 2018</p>
+          <p className="font-semibold">Institution</p>
+          <p className=" mt-2 font-medium">{sampletext}</p>
+        </div>
+      </div>
     </>
   )
 }
